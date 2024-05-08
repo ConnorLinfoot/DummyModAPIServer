b3781f35ac8199906f69ee09bc04af97612fb04d
Connor Linfoot <connorlinfoot@me.com>
2024-05-08 03:04:01 +0100

    Update PartyInfoHandler

--- src/main/java/dev/linfoot/dummymodapiserver/handler/impl/PartyInfoHandler.java
+++ src/main/java/dev/linfoot/dummymodapiserver/handler/impl/PartyInfoHandler.java
@@ -1,23 +1,24 @@
 package dev.linfoot.dummymodapiserver.handler.impl;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableMap;
 import dev.linfoot.dummymodapiserver.DummyModAPIServer;
 import dev.linfoot.dummymodapiserver.handler.PacketHandler;
 import net.hypixel.modapi.packet.impl.clientbound.ClientboundPartyInfoPacket;
 import net.hypixel.modapi.packet.impl.serverbound.ServerboundPartyInfoPacket;
 import org.bukkit.entity.Player;
 
-import java.util.UUID;
-
 public class PartyInfoHandler implements PacketHandler<ServerboundPartyInfoPacket> {
     @Override
     public void handlePacket(DummyModAPIServer server, Player player, ServerboundPartyInfoPacket packet) {
         server.sendPacket(player, new ClientboundPartyInfoPacket(
+                packet.getVersion(),
                 true,
-                player.getUniqueId(),
-                ImmutableSet.of(
+                ImmutableMap.of(
                         player.getUniqueId(),
-                        UUID.randomUUID()
+                        new ClientboundPartyInfoPacket.PartyMember(
+                                player.getUniqueId(),
+                                ClientboundPartyInfoPacket.PartyRole.LEADER
+                        )
                 )
         ));
     }
